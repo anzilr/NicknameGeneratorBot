feebbbb172c9ee6e24923bcdfc4bc51e7da6e9b8
Anzil R <anzilraheem@gmail.com>
2023-04-14 22:57:53 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+START_TEXT = '<b>Hi! I am 🤖 NicknameGeneratorBot. \n\nI can generate nicknames for you 😎.\n\nSend "/generate" or click the "Generate" button to start</b>'
